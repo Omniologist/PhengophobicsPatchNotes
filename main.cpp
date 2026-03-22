e5093b571401029bde41c3337012da84feab2988
Omniologist <sjtciu@gmail.com>
2026-03-22 16:45:50 +1030

    feat: Added init, loadMedia and Close functions. Added main loop
    Followed LazyFoo's guide to SDL3.
    init: intializes window and screen surface.
    loadmedia: load bmp image to a surface
    main loop: looks out for quit event to close program.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,98 @@
-#include <iostream>
+#include <complex>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <string>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+constexpr int kScreenWidth{480};
+constexpr int kScreenHeight{640};
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window* gWindow{ nullptr };
+SDL_Surface* gScreenSurface{ nullptr };
+SDL_Surface* gHelloWorld{ nullptr };
+
+bool init() {
+
+    bool success{ true };
+
+    if ( SDL_Init( SDL_INIT_VIDEO ) == false ) {
+        SDL_Log( "SDL could not intialize! SDL error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else {
+        if (gWindow = SDL_CreateWindow("Phengophobics Patch Notes", kScreenWidth, kScreenHeight, 0 ); gWindow == nullptr) {
+            SDL_Log( "Window could not be created! SDL error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else {
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
     }
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    return success;
+}
+
+bool loadMedia() {
+    bool success{ true };
+    std::string imagePath{ "assets/test.bmp" };
+    if ( gHelloWorld = SDL_LoadBMP(imagePath.c_str() ); gHelloWorld == nullptr ) {
+        SDL_Log( "Unable to load image %s! SDL Error: %s\n", imagePath.c_str(), SDL_GetError() );
+        success = false;
+    }
+
+    return success;
+}
+
+void close() {
+    //Clean up surface
+    SDL_DestroySurface( gHelloWorld );
+    gHelloWorld = nullptr;
+
+    SDL_DestroyWindow( gWindow );
+    gWindow = nullptr;
+    gScreenSurface = nullptr;
+
+    SDL_Quit();
+}
+
+int main( int argc, char* args[] ) {
+    int exitCode{ 0 };
+
+    if ( init() == false ) {
+        SDL_Log( "Unable to intialize program!\n" );
+        exitCode = 1;
+    }
+
+    else {
+        if ( loadMedia() == false ) {
+            SDL_Log( "Unable to load media\n" );
+            exitCode = 2;
+        }
+        else {
+            bool quit{ false };
+            SDL_Event e;
+            SDL_zero( e );
+
+            while ( quit == false ) {
+                while ( SDL_PollEvent( &e ) == true ) {
+                    if ( e.type == SDL_EVENT_QUIT ) {
+                        quit = true;
+                    }
+                }
+
+                SDL_FillSurfaceRect( gScreenSurface, nullptr, SDL_MapSurfaceRGB( gScreenSurface, 0xFF, 0xFF, 0xFF ) );
+
+                SDL_BlitSurface( gHelloWorld, nullptr, gScreenSurface, nullptr);
+
+                SDL_UpdateWindowSurface( gWindow );
+            }
+        }
+
+        close();
+
+        return exitCode;
+    }
 }
